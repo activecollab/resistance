88ad9192c97b2926c132d2ec89996070b2a7d230
Ilija Studen <ilija.studen@activecollab.com>
2015-06-03 09:41:56 +0200

    Make sure that we are connected to the cluster

--- test/src/ActiveCollab/Resistance/TestCase.php
+++ test/src/ActiveCollab/Resistance/TestCase.php
@@ -14,8 +14,10 @@
     public function setUp()
     {
       if (getenv('TEST_REDIS_CLUSTER')) {
+        print "Resistance: Connecting to Redis Cluster...\n";
         Resistance::connectToCluster([ '127.0.0.1:30001', '127.0.0.1:30002', '127.0.0.1:30003', '127.0.0.1:30004', '127.0.0.1:30005' ]);
       } else {
+        print "Resistance: Connecting to Standalone Redis...\n";
         Resistance::connect();
       }
 
